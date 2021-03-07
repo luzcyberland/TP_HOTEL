96802e9290f515fa3967a7ee79efd960fb9a7386
luzCyberland <frozentorture@gmail.com>
2021-03-06 21:20:36 -0300

    Creacion de tablas y constraints

--- parte1.sql
+++ parte1.sql
@@ -24,15 +24,14 @@ GRANT DBA TO BASETP WITH ADMIN OPTION;
 conn BASETP@XEPDB1
 
 
-/**CREACION DE LAS TABLAS **/
-
-/* CREACION DE TABLAS */
+/* -----------------------------CREACION DE TABLAS -----------------------------*/
 CREATE TABLE RESERVA(
  CODIGO NUMBER (12) GENERATED ALWAYS AS IDENTITY 
  START WITH 1 
 MINVALUE 1
 MAXVALUE 99999999
-INCREMENT BY 1 NOCACHE NOCYCLE,
+INCREMENT BY 1 NOCACHE NOCYCLE
+CONSTRAINT PK_RESERVA PRIMARY KEY,
 FECHA_RESERVA DATE NOT NULL,
 HUESPED_TITULAR NUMBER (10) NOT NULL,
 MONTO_TOTAL NUMBER(10)NOT NULL,
@@ -42,16 +41,25 @@ CHECK_OUT DATE NOT NULL,
 NUM_HABITACION NUMBER (5) NOT NULL,
 ESTADO VARCHAR2(1) NOT NULL,
 CODIGO_CANAL NUMBER(5) NOT NULL
+TABLESPACE BASEDATOSTP
+STORAGE (INITIAL 639 K )
 );
 
 CREATE TABLE CARGO_RESERVA(
-CODIGO_CARGO NUMBER(10) NOT NULL,
+CODIGO_CARGO NUMBER(10) GENERATED ALWAYS AS IDENTITY 
+ START WITH 1 
+MINVALUE 1
+MAXVALUE 99999999
+INCREMENT BY 1 NOCACHE NOCYCLE
+CONSTRAINT PK_CARGO_RESERVA PRIMARY KEY,
 CODIGO_RESERVA NUMBER (12) NOT NULL,
 ID_PROD_SERV NUMBER (5) NOT NULL,
 PRECIO_UNITARIO NUMBER(10) NOT NULL,
 CANTIDAD NUMBER (5) NOT NULL,
-APLICAR_COBRO VARCHAR2(1) NOT NULL,
+APLICAR_COBRO VARCHAR2(1) DEFAULT 'S' NOT NULL,
 CODIGO_FACTURA NUMBER(5)
+TABLESPACE BASEDATOSTP
+STORAGE (INITIAL 21569 K )
 );
 
 CREATE TABLE OCUPANTE (
@@ -59,4 +67,110 @@ CODIGO_OCUPANTE NUMBER(10) NOT NULL,
 CODIGO_RESERVA NUMBER(12) NOT NULL,
 FEC_HORA_ENTRADA DATE NOT NULL,
 FEC_HORA_SALIDA DATE,
+TABLESPACE BASEDATOSTP
+STORAGE (INITIAL 328 K )
 );
+
+
+/*--------------------------------CONSTRAINTS----------------------------------
+
++-----------------------------------------+
+|                 RESERVA                 |
++-----------------------------------------+
+| PK_RESERVA (CODIGO)                     |
+| CANAL_RESERVA_RESERVA_FK (CODIGO_CANAL) |
+| HABITACION_RESERVA_FK (NUM_HABITACION)  |
+| HUESPUED_RESERVA_FK (HUESPED_TITULAR)   |
++-----------------------------------------+*/
+
+alter table RESERVA
+  add constraint CANAL_RESERVA_RESERVA_FK foreign key (CODIGO_CANAL)
+  references CANAL_RESERVA (CODIGO);
+
+ alter table RESERVA
+  add constraint HABITACION_RESERVA_FK foreign key (NUM_HABITACION)
+  references HABITACION (NUMERO);
+ 
+ alter table RESERVA
+  add constraint HUESPED_RESERVA_FK foreign key (HUESPED_TITULAR)
+  references HUESPED (CODIGO);
+ 
+ alter table RESERVA 
+  add constraint CHKFECHA CHECK (CHECK_OUT > CHECK_IN)
+  
+ alter table RESERVA 
+  add constraint CHKESTADO CHECK (ESTADO = 'P' OR 
+                                (ESTADO = 'C' AND SALDO_ABONAR = 0.5 * SALDO_TOTAL)
+  								OR (ESTADO = F AND CHECK_OUT IS NOT NULL ) OR ESTADO = 'X')
+ 
+  
+/*-----------------------------------------------------------------------------
++-----------------------------------------------+
+|                 CARGO_RESERVA                 |
++-----------------------------------------------+
+| PK_CARGO_RESERVA (CODIGO_CARGO)               |
+| FACTURA_CONSUMO_RESERVA_FK (CODIGO_FACTURA)   |
+| PRODUCTO_SERVICIO_CONSUMO_R199 (ID_PROD_SERV) |
+| RESERVA_CONSUMO_RESERVA_FK (CODIGO_RESERVA))  |
++-----------------------------------------------+*/
+
+  
+  alter table CARGO_RESERVA
+  add constraint FACTURA_CONSUMO_RESERVA_FK foreign key (CODIGO_FACTURA)
+  references FACTURA_VENTAS (CODIGO);
+ 
+  alter table CARGO_RESERVA
+  add constraint PRODUCTO_SERVICIO_CONSUMO_R199 foreign key (ID_PROD_SERV)
+  references PRODUCTO_SERVICIO (ID);
+  
+   alter table CARGO_RESERVA
+  add constraint RESERVA_CONSUMO_RESERVA_FK foreign key (CODIGO_RESERVA)
+  references RESERVA (CODIGO);
+  
+  alter table CARGO_RESERVA 
+  add constraint CHK_APLICAR_COBRO CHECK ((APLICAR_COBRO = 'N' AND CODIGO_FACTURA IS NOT NULL) OR 
+  										  APLICAR_COBRO = 'S');
+  											
+ 
+/*-----------------------------------------------------------------------------
+ * 
++---------------------------------------------------+
+|                     OCUPANTE                      |
++---------------------------------------------------+
+| PK_COD_OCUPANTE (CODIGO_OCUPANTE, CODIGO_RESERVA) |
+| HUESPUED_OCUPANTE_FK (CODIGO_OCUPANTE)            |
+| RESERVA_OCUPANTE_FK (CODIGO_RESERVA)              |
++---------------------------------------------------+
+ */ 
+ 
+ alter table OCUPANTE
+  add constraint HUESPED_OCUPANTE_FK foreign key (CODIGO_OCUPANTE)
+  references HUESPED (CODIGO);
+ 
+ alter table OCUPANTE
+  add constraint RESERVA_OCUPANTE_FK foreign key (CODIGO_OCUPANTE)
+  references RESERVA (CODIGO);
+ 
+ alter table OCUPANTE
+  add constraint PK_COD_OCUPANTE primary key (CODIGO_OCUPANTE, CODIGO_RESERVA);
+ 
+  alter table RESERVA 
+  add constraint CHK_FECHA_SALIDA CHECK (FEC_HORA_SALIDA > FEC_HORA_ENTRADA);
+ 
+ 
+ 
+ /*-----------------------------------PARTE B--------------------------------*/
+ 
+create sequence SEC_PAGO_ITEM
+	increment by 1
+	start with 1
+	maxvalue 99999
+	nocache
+	nocycle;
+
+ 
+ 
+ 
+  
+  
+  
